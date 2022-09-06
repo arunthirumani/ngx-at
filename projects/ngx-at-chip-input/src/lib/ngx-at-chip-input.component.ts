05d1f7bafb2d62c63392f2510b46ddf914eaa20b
arunthirumani <arun.tirumani@gmail.com>
2022-09-06 17:51:19 +0530

    Implement control value accessor for chip input

--- projects/ngx-at-chip-input/src/lib/ngx-at-chip-input.component.ts
+++ projects/ngx-at-chip-input/src/lib/ngx-at-chip-input.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { ChangeDetectorRef, Component, forwardRef, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'ngx-at-chip-input',
   templateUrl: './ngx-at-chip-input.component.html',
-  styleUrls: ['./ngx-at-chip-input.component.scss']
+  styleUrls: ['./ngx-at-chip-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => NgxAtChipInputComponent),
+    multi: true
+  }, {
+    provide: NG_VALIDATORS,
+    useExisting: forwardRef(() => NgxAtChipInputComponent),
+    multi: true
+  }]
 })
-export class NgxAtChipInputComponent implements OnInit {
+export class NgxAtChipInputComponent implements OnInit, ControlValueAccessor {
 
   chipInput = new FormControl();
   chipList = new FormControl();
@@ -14,20 +23,54 @@ export class NgxAtChipInputComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.chipList.setValue(['test1', 'long string', 'test2']);
+    this.chipList.setValue([]);
   }
 
   addChipHandler() {
     const chip = this.chipInput.value;
     const chipList = this.chipList.value;
-    chipList.push(chip);
+    const chipListClone = chipList.slice();
+    chipListClone.push(chip);
+    this.chipList.setValue(chipListClone);
     this.chipInput.setValue('');
   }
 
   removeChipHandler(index) {
-    const chip = this.chipInput.value;
     const chipList: Array<string> = this.chipList.value;
-    chipList.splice(index, 1);
+    const chipListClone = chipList.slice();
+    chipListClone.splice(index, 1);
+    this.chipList.setValue(chipListClone);
+  }
+
+  writeValue(value) {
+    if (value) {
+      this.chipList.setValue(value);
+    } else {
+      this.chipList.setValue([]);
+    }
+  }
+
+  registerOnChange(fn) {
+    this.chipList.valueChanges.subscribe((value) => {
+        fn(value);
+    });
+  }
+
+  setDisabledState(isDisabled) {
+    if (isDisabled) {
+      this.chipInput.disable();
+      this.chipList.disable();
+    } else {
+      this.chipList.enable();
+      this.chipList.enable();
+    }
+  }
+
+  validate(): ValidationErrors | null {
+    return null;
+  }
+
+  registerOnTouched(fn) {
   }
 
 }
--- projects/ngx-at-chip-input/src/lib/ngx-at-chip-input.module.ts
+++ projects/ngx-at-chip-input/src/lib/ngx-at-chip-input.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxAtChipInputComponent } from './ngx-at-chip-input.component';
 
