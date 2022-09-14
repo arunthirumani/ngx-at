07773e119315b421fdd1e7c986a82b1ccb355b35
arunthirumani <arun.tirumani@gmail.com>
2022-09-14 22:18:12 +0530

    Initial commit of unit test for ngx-chip-input

--- projects/ngx-at-chip-input/src/lib/ngx-at-chip-input.component.spec.ts
+++ projects/ngx-at-chip-input/src/lib/ngx-at-chip-input.component.spec.ts
@@ -1,25 +1,52 @@
+import { Component, DebugElement, OnInit, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { NgxAtChipInputComponent } from './ngx-at-chip-input.component';
 
 describe('NgxAtChipInputComponent', () => {
-  let component: NgxAtChipInputComponent;
-  let fixture: ComponentFixture<NgxAtChipInputComponent>;
+  let component: TestChipInput;
+  let fixture: ComponentFixture<TestChipInput>;
+  let inputDebugElement: DebugElement;
+  let inputNativeElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NgxAtChipInputComponent ]
+      declarations: [NgxAtChipInputComponent, TestChipInput],
+      imports: [ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NgxAtChipInputComponent);
+    fixture = TestBed.createComponent(TestChipInput);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    inputDebugElement = fixture.debugElement.query(By.directive(NgxAtChipInputComponent));
+    inputNativeElement = inputDebugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Enter keyup should invoke addChipHandler()', () => {
+    spyOn(component.chipInput, 'addChipHandler');
+    const inputBox = inputNativeElement.querySelector('input');
+    inputBox.value="test";
+    const event = new KeyboardEvent("keyup", {
+      'key': 'Enter'
+    });
+    inputBox.dispatchEvent(event);    
+    expect(component.chipInput.addChipHandler).toHaveBeenCalled();
   });
 });
+
+@Component({
+  template: `<ngx-at-chip-input [formControl]="chipList"></ngx-at-chip-input>`
+})
+class TestChipInput implements OnInit {
+  @ViewChild(NgxAtChipInputComponent, { static: false }) chipInput: NgxAtChipInputComponent;
+  chipList = new FormControl();
+  ngOnInit() {
+    this.chipList.setValue(['test1', 'chip1', 'another chip']);
+  }
+}
